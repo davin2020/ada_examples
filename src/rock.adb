f3ffe94e367bf0ff054d50d1fde7e32ba5bb29bd
Davin Stirling <davin.stirling@capgemini.com>
2022-11-12 01:16:42 +0000

    user choses an item, then computer chooses via random number generator

--- src/rock.adb
+++ src/rock.adb
@@ -1,28 +1,44 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Ada.Strings.Unbounded;
+with Ada.Strings.Unbounded; -- to create variable length strings
+with ada.numerics.discrete_random;
 
 procedure Rock is
    use Ada.Strings.Unbounded;
-   type Index is range 1 .. 5;
+   User_Choice: Integer := 0 ;
 
+   subtype VariableLengthString is Unbounded_String;
+   type array_strings is array (1 .. 3) of VariableLengthString;
+   -- IDE doesnt have line/word wrap option but u can add carraige returns manually
+   options_array : array_strings := (To_Unbounded_String("rock"),
+                              To_Unbounded_String("paper"),
+                              To_Unbounded_String("scissors"));
 
-   subtype str_data is String (1 .. 3);
-   type str_array is array (1 .. 3) of str_data;
+   -- setup random num generator
+   subtype randRange is Integer range 1..3;
+   package Rand_Int is new ada.numerics.discrete_random (randRange);
+   use Rand_Int;
+   gen : Generator;
+   num: randRange;
 
-   vals : constant str_array := ("roc", "pap", "sci");
-
-   subtype VString is Unbounded_String;
-   type options_array is array (1 .. 3) of VString;
-   values : options_array := (To_Unbounded_String("rock"), To_Unbounded_String("paper"), To_Unbounded_String("scissors"));
 
 begin
-   --  Insert code here.
-   Put_Line("Enter a number 1 Rock, 2 Paper 3 Scissors");
-   null;
-   -- read stuff
+   -- Next - add a loop to keep playing eg best of 3, or determine the winner
 
-   -- print stuff
-   for Item of vals loop
-      Put_Line(Item);
-   end loop;
+   --  Insert code here.
+   Put_Line("Enter a number 1 for Rock, 2 for Paper, 3 for Scissors");
+
+   -- read stuff - causing errors, need to convert string to int
+   User_Choice := Integer'Value(Get_Line);
+
+   --print the to_string version of the users requested index of the options_array
+   Put_Line("You chose: ");
+   Put_Line(To_String(options_array(User_Choice)));
+
+   --randomly choose an option for the computer user, then dispaly its number and value from the array
+   reset(gen);
+   num := random(gen);
+   Put_Line ("The computer used random number gen to choose: ");
+   put_line(randRange'Image(num));
+   Put_Line(To_String(options_array(num)));
+
 end Rock;
