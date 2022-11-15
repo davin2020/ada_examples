10832103f0db07cb72eba147191aab8a8c3aa811
Davin Stirling <davin.stirling@capgemini.com>
2022-11-15 15:42:29 +0000

    changed Winner to unbounded string and renamed input params for Determine_winner

--- src/rock.adb
+++ src/rock.adb
@@ -3,7 +3,7 @@ with Ada.Strings.Unbounded; -- to create variable length strings
 with Ada.Numerics.Discrete_Random;
 
 --do i need to put things in a RockGame package and use a .ads spec file?
--- .ads file would likely only contain this proc, and NOT the sub-fucntion Find_Winner()
+-- .ads file would likely only contain this proc, and NOT the sub-fucntion Determine_Winner()
 -- procedure Rock;
 
 -- FYI Parameters in Ada subprogram declarations are separated with semicolons,
@@ -15,8 +15,9 @@ procedure Rock is
    User_Choice : Integer := 0;
 
    --  Winner : Unbounded_String;
-   Winner : String (1 .. 8);
-   Result : String (1 .. 8);
+   Winner, WinnerResult : Unbounded_String;
+   --  Winner          : String (1 .. 8);
+   --  Result : String (1 .. 8);
 
    ROCK     : constant Integer := 1;
    PAPER    : constant Integer := 2;
@@ -32,6 +33,7 @@ procedure Rock is
    options_array : array_strings :=
      (To_Unbounded_String ("rock"), To_Unbounded_String ("paper"),
       To_Unbounded_String ("scissors"));
+   -- try enum instead for options
 
    -- setup random num generator
    subtype randRange is Integer range 1 .. 3;
@@ -40,80 +42,65 @@ procedure Rock is
    gen             : Generator;
    Computer_Choice : randRange;
 
-   --should this be a sub procedure or not? YES
-   --  procedure Determine_Winner is
-   --  begin
-   --     if (User_Choice = PAPER and Computer_Choice = ROCK) or
-   --       (User_Choice = ROCK and Computer_Choice = SCISSORS) or
-   --       (User_Choice = SCISSORS and Computer_Choice = PAPER)
-   --     then
-   --        -- Strings must be of fixed length, hence currently padded, but Unbounded_String dont have to be
-   --        Winner := "User    ";
-   --     elsif (User_Choice = ROCK and Computer_Choice = ROCK) or
-   --       (User_Choice = PAPER and Computer_Choice = PAPER) or
-   --       (User_Choice = SCISSORS and Computer_Choice = SCISSORS)
-   --     then
-   --        Winner := "Tie!    ";
-   --     else
-   --        Winner := "Computer";
-   --     end if;
-   --  end Determine_Winner;
-
-
-
    -- FunctioNs Need to return a value while ProceduREs REturn nothing
-   -- TODO shoudl input params have diff names ?
-      function Find_Winner(User_Choice1 : Integer; Computer_Choice : Integer) return String is
-      begin
-         if (User_Choice1 = PAPER and Computer_Choice = ROCK) or
-           (User_Choice1 = ROCK and Computer_Choice = SCISSORS) or
-           (User_Choice1 = SCISSORS and Computer_Choice = PAPER)
-         then
-            -- Strings must be of fixed length, hence currently padded, but Unbounded_String dont have to be
-            Winner := "User_   ";
-         elsif (User_Choice1 = ROCK and Computer_Choice = ROCK) or
-           (User_Choice1 = PAPER and Computer_Choice = PAPER) or
-           (User_Choice1 = SCISSORS and Computer_Choice = SCISSORS)
-         then
-            Winner := "Tie!_   ";
+   -- do i need to add OUT params, if so how? -- Determine_Winner
+   -- function Find_Winner(User_Input : in Integer; Computer_Choice : in  Integer; Result : out String) return String is
+   function Determine_Winner
+     (User_Input : in Integer; Computer_Input : in Integer) return Unbounded_String is
+   begin
+      if (User_Input = PAPER and Computer_Input = ROCK) or
+        (User_Input = ROCK and Computer_Input = SCISSORS) or
+        (User_Input = SCISSORS and Computer_Input = PAPER) then
+         -- Strings must be of fixed length, hence currently padded, but Unbounded_String dont have to be
+         --  Winner := "User_   ";
+         -- how to assing an unbounded string when your value IS a string ie no implicit casting/type juggling happens
+         Winner := To_Unbounded_String ("User");
+      elsif (User_Input = ROCK and Computer_Input = ROCK) or
+        (User_Input = PAPER and Computer_Input = PAPER) or
+        (User_Input = SCISSORS and Computer_Input = SCISSORS)
+      then
+         --  Winner := "Tie!_   ";
+         Winner      := To_Unbounded_String ("Tie");
       else
-            -- TODO use var length strings
-            Winner := "Compute_";
-         end if;
-         return Winner;
-      end Find_Winner;
+         -- TODO use var length strings
+         --  Winner := "Compute_";
+         Winner      := To_Unbounded_String ("Computer");
+      end if;
+      return Winner;
+   end Determine_Winner;
+
 
 
 begin
    -- Next - keep score eg  best of 3 across multiple rounds and then determine the winner
    Until_Loop :
    loop
-      Put_Line ("Enter a number - 1 for Rock, 2 for Paper, 3 for Scissors, or 0 to exit");
+      Put_Line ("Enter a number - 1 for Rock, 2 for Paper, 3 for Scissors - or 0 to exit");
 
       -- read str input and convert to int
       User_Choice := Integer'Value (Get_Line);
       exit Until_Loop when User_Choice = 0;
 
       --print the to_string version of the users requested index of the options_array
-      Put_Line ("You chose: " & To_String(options_array(User_Choice)));
+      Put_Line ("You chose: " & To_String (options_array (User_Choice)));
       --  Put_Line (To_String (options_array(User_Choice)));
 
       --randomly choose an option for the computer user, then dispaly its number and value from the array
       Reset (gen);
       Computer_Choice := Random (gen);
-      Put_Line ("The computer used random number gen to choose: " & To_String(options_array (Computer_Choice)));
+      Put_Line ("The computer used random number generator to choose: " &
+         To_String (options_array (Computer_Choice)));
       --  Put_Line (randRange'Image (Computer_Choice)); -- converts an int to string for printing
       --  Put_Line (To_String (options_array (Computer_Choice)));
 
       --  2paper beats 1rock , 1rock beats 3scissors, 3sciossrs beats 2paper. - RPS
       --  if user=2 and computer=1 OR user=1 and computer=3 or user=3 and computer=2 - then user wins
 
-      -- determine winer was here
-      --  Determine_Winner;
 
-      --this works ok
-      Result := Find_Winner(User_Choice, Computer_Choice);
-      Put_Line ("The RESULT winner is: " & Result);
+      --this works ok, and is now unounded string
+      WinnerResult := Determine_Winner (User_Choice, Computer_Choice);
+      Put_Line ("...and the winner is: " & To_String (WinnerResult));
+      --  Put_Line ("The RESULT winner is: " & Result);
 
       --why/how is winner still begin caclultaed? cos its alocal var and thsu can still ebaccesssed !
       --  Put_Line ("The winner is: " & Winner);
